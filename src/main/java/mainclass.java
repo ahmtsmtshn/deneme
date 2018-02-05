4f8f2c6b206b97c1ccdc34150cd6f0aecaa640cd
ahmtsmtshn <36156775+ahmtsmtshn@users.noreply.github.com>
2018-02-06 00:55:28 +0300

    S

--- src/main/java/mainclass.java
+++ src/main/java/mainclass.java
@@ -2,6 +2,6 @@ public class mainclass {
 
     public static void main(String[] args) {
             testClass t = new testClass();
-            System.out.print(t.topla1(3,4));
+            System.out.print(t.topla1(3,5));
     }
 }
