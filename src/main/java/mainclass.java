bc81ab602ef3f68708cb45eb9ae332a6adb9a673
ahmtsmtshn <36156775+ahmtsmtshn@users.noreply.github.com>
2018-02-06 00:51:15 +0300

    PROJECTEDDIT
    WASDGFHGJ

---
+++ src/main/java/mainclass.java
@@ -0,0 +1,7 @@
+public class mainclass {
+
+    public static void main(String[] args) {
+            testClass t = new testClass();
+            System.out.print(t.topla1(3,4));
+    }
+}
---
+++ src/main/java/testClass.java
@@ -0,0 +1,9 @@
+public class testClass {
+
+    private int topla(int a,int b)
+    {return a+b;}
+
+    public int topla1(int a,int b)
+    {return topla(a,b);}
+
+}
